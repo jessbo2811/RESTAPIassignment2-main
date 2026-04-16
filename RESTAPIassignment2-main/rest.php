ba44c5863a416ca62c4e39a586dd9d442cf18fe1
Jess Bryant <jessmaybryantt@gmail.com>
2026-04-16 10:13:51 +0100

    connected the php file to the mySQL db

--- RESTAPIassignment2-main/rest.php
+++ RESTAPIassignment2-main/rest.php
@@ -1,3 +1,17 @@
 <?php
+$servername = "brighton";
+$username = "jmb181_commentuser";
+$password = "str0ngpassw0rd";
+$db = "jmb181_VAMuseumComments";
 
+$mysqli = new mysqli($servername, $username, $password, $db);
+
+if ($mysqli->connect_errno) {
+    printf("Connection failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    print("successful!");
+}
+
+$mysqli->close();
 ?>
