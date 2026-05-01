5660845c0c3f30b8040cd49ec55244abcb38b952
Jess Bryant <jessmaybryantt@gmail.com>
2026-05-01 02:28:03 +0100

    built the Create() function and the HandleRequest() function

--- RESTAPIassignment2-main/api.php
+++ RESTAPIassignment2-main/api.php
@@ -16,22 +16,55 @@ class API {
         );
 
         if ($this->conn->connect_errno) {
-            die("Connection failed: " . $this->conn->connect_error);
+            http_response_code(500);
+            exit;
         }    
     }
 
     public function HandleRequest() {
+        header("Content-Type: application/json; charset=UTF-8");
         $method = $_SERVER['REQUEST_METHOD'];
         if ($method === 'GET') {
-
+            $this->Read();
         }
 
         else if ($method === 'POST') {
-
+           $this->Create();
         }
 
         else {
-
+            http_response_code(405);
+            exit;
         }
     }
-}
+
+    public function Create() {
+        $oid = $_POST['oid'];
+        $name = $_POST['name'] ?? null;
+        $comment = $_POST['comment'];
+
+        if (empty($oid) || strlen($oid) > 32 || !ctype_alnum($oid)) { {
+            http_response_code(400);
+            exit;
+        }
+
+        if (!empty($name) && $name->strlen() > 64) {
+            http_response_code(400);
+            exit;
+        }
+        
+        if (empty($comment)) {
+            http_response_code(400);
+            exit;
+        }
+
+        $sql = "INSERT INTO tComments (objectId, name, comment) VALUES ('$oid', '$name', '$comment')";
+        http_response_code(201);
+        exit;
+    }    
+
+    public function Read() {
+
+    }
+    }
+
